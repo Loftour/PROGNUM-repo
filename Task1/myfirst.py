cbf838d54496bdceb03197e4016676f9c1f56f11
M.C. Lavik <mclavik@virgo07.intra.astro.rug.nl>
2026-02-19 14:28:14 +0100

    week 2 update

---
+++ Task1/myfirst.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Answers for Week 1
+# 
+# * Name: Michael Lavik
+# * Username: mclavik
+# * Student s-number: S-6452914
+# * Group (AS1, etc.): AS4
+
+# In[1]:
+
+
+x=22/7
+
+
+# $$y=\frac{sin(x)}{x}$$
+
+# In[19]:
+
+
+pi = 22/7 # Processed on Jupyter Hub
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+x = 6
+print(x)
+x*=5
+print(x)
+x*=4
+print(x)
+x*=3
+print(x)
+x*=2
+print(x)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+e=1.6*10**-19
+print(e)
+f=2.42*10**28
+print(f)
+c=3*10**8
+print(c)
+h=6.63*10**-34
+print(h)
+
+lam=c/f
+print(lam)
+
+E=(h*c)/lam
+print(E)
+
+ev=E/e
+print(ev)
+
+
+# In[ ]:
+
+
+
+
+
+# In[20]:
+
+
+from scipy.constants import G
+M1=5.9772*10**24
+M2=7.342*10**22
+r=385.0006*10**6
+F=(G*M1*M2)/r**2
+print(F)
+
+
+# In[ ]:
+
+
+
+
